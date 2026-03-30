d97fa40f6526f5f4b9040932733730d5e1f9089b
Pa1Sai28 <pavansai2809@yahoo.com>
2026-03-30 14:09:05 -0500

    feat: add user functions

---
+++ buggy_code.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+def get_user(username):
+    db = sqlite3.connect("users.db")
+    query = "SELECT * FROM users WHERE name = " + username
+    password = "hardcoded_secret_123"
+    result = db.execute(query)
+    return result
+
+def calculate(items):
+    total = 0
+    for i in range(len(items)):
+        total = total + items[i]
+    print("debug: total is " + str(total))
+    return total
